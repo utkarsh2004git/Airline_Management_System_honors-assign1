e2a504125108929ceaf793fa24a2ac04d4f096a5
ukarsh2004git <utkarshshahare2004@gmail.com>
2025-03-15 22:54:53 +0530

    first commit

---
+++ user-service/src/main/java/com/rcoem/devops/UserController.java
@@ -0,0 +1,51 @@
+package com.rcoem.devops;
+import com.rcoem.devops.dto.UserInfo;
+import com.rcoem.devops.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    List<UserInfo> getAllUsers(){
+          return userService.getAllUsers();
+    }
+
+
+    @PostMapping
+    UserInfo createUser(@RequestBody UserInfo userInfo){
+        return userService.createUser(userInfo);
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<UserInfo>  getUserById(@PathVariable String id){
+
+        try{
+            return ResponseEntity.ok(userService.getUser(id));
+        }catch(Exception e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+//    @PutMapping("/{id}/{ticketId}")
+//    ResponseEntity<String> updateUser(@PathVariable String id, @PathVariable String ticketId){
+//
+//
+//        try{
+//            userService.updateUserTicketList(id, ticketId);
+//            return ResponseEntity.ok("User ticket Added");
+//        }catch(Exception e){
+//            return ResponseEntity.notFound().build();
+//        }
+//    }
+
+}
---
+++ user-service/src/main/java/com/rcoem/devops/repository/UserInfoRepository.java
@@ -0,0 +1,58 @@
+package com.rcoem.devops.repository;
+
+import com.rcoem.devops.dto.UserInfo;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Component
+public class UserInfoRepository {
+
+    Map<String, UserInfo> userInfoTable;
+
+
+    @PostConstruct
+    public void init() {
+        userInfoTable = new HashMap<>();
+    }
+
+
+    public List<UserInfo> getAllUsers(){
+        return userInfoTable.values()
+                .stream().collect(Collectors.toList());
+    }
+
+    public UserInfo createUser(UserInfo userInfo) {
+        String userId = UUID.randomUUID().toString();
+        UserInfo updatedUser = userInfo.toBuilder().id(userId).build();
+        this.userInfoTable.put(userId, updatedUser);
+        return updatedUser;
+    }
+
+
+    public UserInfo getUserById(String userId){
+        return userInfoTable.get(userId);
+    }
+
+//    public void updateUserTicketList(String userId, String ticketId) {
+//        UserInfo userInfo = userInfoTable.get(userId);
+//        if (userInfo != null) {
+//            // Ensure ticket list is initialized
+//            if (userInfo.getBookedTicketIds() == null) {
+//                userInfo.setBookedTicketIds(new ArrayList<>());
+//            }
+//
+//            // Add ticketId if it's not already in the list
+//            if (!userInfo.getBookedTicketIds().contains(ticketId)) {
+//                userInfo.getBookedTicketIds().add(ticketId);
+//            }
+//
+//            // Put the updated user info back into the map
+//            userInfoTable.put(userId, userInfo);
+//        }
+//    }
+
+
+}
